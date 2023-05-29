5e926c391f2630a50423ea6b5864d22cc2578e88
rudsa <rudsa@naver.com>
2023-05-29 23:17:25 +0900

    react-context-dynamic

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,20 +1,27 @@
-import ColorContext from "../contexts/color";
+import { ColorConsumer } from "../contexts/color";
 
 const ColorBox = () => {
   return (
-    <div>
-      <ColorContext.Consumer>
-        {(value) => (
+    <ColorConsumer>
+      {({ state }) => (
+        <>
           <div
             style={{
               width: "64px",
               height: "64px",
-              background: value.color,
+              background: state.color,
             }}
           />
-        )}
-      </ColorContext.Consumer>
-    </div>
+          <div
+            style={{
+              width: "32px",
+              height: "32px",
+              background: state.subcolor,
+            }}
+          />
+        </>
+      )}
+    </ColorConsumer>
   );
 };
 
