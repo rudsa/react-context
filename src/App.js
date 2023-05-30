231c6a595a70ec804c666fde86e2298d352dd4e0
zmainsk <skkim@j2stechno.com>
2023-05-30 10:31:31 +0900

    react-context-staticContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ColorBox from "./components/ColorBox";
 import { ColorProvider } from "./contexts/color";
+import SelectColor from "./components/SelectColor";
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColor />
         <ColorBox />
       </div>
     </ColorProvider>
